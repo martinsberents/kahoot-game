157adff939aa2144f6c4079d786e2990d68c347e
Martins Berents <martins.berents@gmail.com>
2018-02-18 15:12:16 +0200

    Add item score displaying and point calculation logic

---
+++ js/main.js
@@ -0,0 +1,129 @@
+(function(){
+    const itemValueMap = {
+        A: {
+            value: 50,
+            bonusValue: 200,
+            bonusNumber: 3
+        },
+        B: {
+            value: 30,
+            bonusValue: 90,
+            bonusNumber: 2
+        },
+        C: {
+            value: 20
+        },
+        D: {
+            value: 15
+        },
+        E: {
+            value: 10
+        }
+    };
+
+    const userStats = {
+        items: {}
+    };
+
+    const calculateScoreWithBonus = function (itemParameters, itemStats) {
+        const itemBonusValue = itemParameters.bonusValue;
+        const itemValue = itemParameters.value;
+        const bonusCount = parseInt(itemStats.quantity / itemParameters.bonusNumber);
+        const remainderCount = itemStats.quantity - itemParameters.bonusNumber * bonusCount;
+
+        return itemBonusValue * bonusCount + itemValue * remainderCount;
+    };
+
+    calculateScoreWithoutBonus = function (itemParameters, itemStats) {
+        return itemStats.score + itemParameters.value;
+    };
+
+    const calculateScore = function (itemParameters, itemStats) {
+        if (itemParameters.bonusNumber) {
+            return calculateScoreWithBonus(itemParameters, itemStats);
+        }
+
+        return calculateScoreWithoutBonus(itemParameters, itemStats);
+    };
+
+    const updateScoreEntry = function (scoreEntry, itemStats) {
+        if (scoreEntry) {
+            return updateExistingScoreEntry(scoreEntry, itemStats);
+        }
+
+        return createNewScoreEntry(itemStats);
+    };
+
+    const updateExistingScoreEntry = function (scoreEntry, itemStats) {
+        const itemQuantity = scoreEntry.querySelector('.item-quantity');
+        const itemScore = scoreEntry.querySelector('.item-score');
+
+        itemQuantity.innerHTML = itemStats.quantity;
+        itemScore.innerHTML = itemStats.score;
+    };
+
+    const createNewScoreEntry = function (itemStats) {
+        const newScoreEntry = document.createElement('div');
+        newScoreEntry.className = 'item-stats three-column-grid rotate-in';
+        newScoreEntry.setAttribute('data-item-score-id', itemStats.id);
+        newScoreEntry.innerHTML = `<span class="grid-cell-center">${itemStats.id}</span> <span class="item-quantity grid-cell-center">${itemStats.quantity}</span> <span class="item-score grid-cell-center">${itemStats.score}</span>`;
+
+        const itemStatsContainer = document.querySelector('.items-stats-content');
+        itemStatsContainer.appendChild(newScoreEntry);
+    };
+
+    const calculateTotalScore = function () {
+        let total = 0;
+        Object.keys(userStats.items).forEach((itemId) => {
+            total += userStats.items[itemId].score;
+        });
+
+        return total;
+    };
+
+    const calculateTotalBonus = function () {
+        let total = 0;
+        Object.keys(userStats.items).forEach((itemId) => {
+            const itemStats = userStats.items[itemId];
+            const itemValue = itemValueMap[itemId].value;
+            const itemCount = itemStats.quantity;
+
+            total += itemStats.score - itemValue * itemCount;
+        });
+
+        return total;
+    };
+
+    const updateTotalScore = function () {
+        const totalScoreElement = document.querySelector('.total-score');
+        totalScoreElement.innerHTML = calculateTotalScore();
+    };
+
+    const updateTotalBonus = function () {
+        const totalBonusElement = document.querySelector('.total-bonus');
+        totalBonusElement.innerHTML = calculateTotalBonus();
+    };
+
+    const itemClickHandler = function (event) {
+        const itemId = event.target.dataset.itemId;
+        const itemParameters = itemValueMap[itemId];
+        const scoreEntry = document.querySelector(`[data-item-score-id=${itemId}]`);
+        const itemStats = userStats.items[itemId] || (userStats.items[itemId] = {id: itemId, quantity: 0, score: 0});
+
+        itemStats.quantity++;
+        itemStats.score = calculateScore(itemParameters, itemStats);
+
+        updateScoreEntry(scoreEntry, itemStats);
+        updateTotalScore();
+        updateTotalBonus();
+    };
+
+    const addClickHandlerToItems = function (items) {
+        for (let item of items) {
+            item.addEventListener('click', itemClickHandler);
+        }
+    };
+
+    const items = document.getElementsByClassName('item');
+    addClickHandlerToItems(items);
+}());
